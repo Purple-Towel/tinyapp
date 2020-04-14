9efddb3dd2db0e2fc5bf32efbd66ca3b3d88163c
Angad Grewal <angadgrewal31@gmail.com>
2020-04-14 16:17:07 +0000

    added framework for inputting URLs to be shortened

--- express_server.js
+++ express_server.js
@@ -2,6 +2,19 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+const generateURL = function() {
+  let characters = "0123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
+  let outputURL = "";
+  for (let i = 6; i > 0; i--) {
+    outputURL += characters[Math.floor(Math.random() * characters.length)];
+  }
+  return outputURL;
+}
+
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -21,9 +34,19 @@ app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("url_index", templateVars);
 });
+
+app.get("/urls/new", (req, res) => {
+  res.render("url_new");
+});
+
 app.get("/urls/:short_url", (req, res) => {
   let templateVars = { shortURL: req.params.short_url, longURL: urlDatabase[`${req.params.short_url}`] };
   res.render("url_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/set", (req, res) => {
