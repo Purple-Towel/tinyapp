d97bf90034991545d136054f25feb086c28b92e2
Angad Grewal <angadgrewal31@gmail.com>
2020-04-15 03:25:45 +0000

    added cookies to manage login/logout operations

--- express_server.js
+++ express_server.js
@@ -2,9 +2,12 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+const cookieParser = require("cookie-parser");
+
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 
 const generateURL = function() {
   let characters = "0123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
@@ -31,16 +34,17 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = { urls: urlDatabase, username: req.cookies["username"] };
   res.render("url_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("url_new");
+  let templateVars = { username: req.cookies["username"] }
+  res.render("url_new", templateVars);
 });
 
 app.get("/urls/:short_url", (req, res) => {
-  let templateVars = { shortURL: req.params.short_url, longURL: urlDatabase[`${req.params.short_url}`] };
+  let templateVars = { shortURL: req.params.short_url, longURL: urlDatabase[`${req.params.short_url}`], username: req.cookies["username"] };
   res.render("url_show", templateVars);
 });
 
@@ -63,10 +67,20 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${shortURLToAdd}`);         
 });
 
-app.get('/u/:short_url', (req, res) => {
+app.get("/u/:short_url", (req, res) => {
  res.redirect(`${urlDatabase[`${req.params.short_url}`]}`);
 });
  
+app.post("/login", (req, res) => {
+  let usernameInput = req.body.username;
+  res.cookie('username', usernameInput);
+  res.redirect("/urls");
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie('username');
+  res.redirect("/urls");
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
